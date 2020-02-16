aef42ffb7346b2e1577324f33528a740cc5eec1c
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2020-02-17 02:03:22 +1100

    Initial api proof of concept

---
+++ api/main.py
@@ -0,0 +1,29 @@
+import glob
+import json
+
+from fastapi import FastAPI
+from slugify import slugify
+from tinydb import TinyDB, where
+from tinydb.storages import MemoryStorage
+
+
+def initialise_db():
+    tinydb = TinyDB(storage=MemoryStorage)
+    for filename in glob.iglob("data/json/ships/*.json"):
+        with open(filename, encoding="utf-8") as f:
+            tinydb.insert(json.load(f))
+    return tinydb
+
+
+db = initialise_db()
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/ships/{slug}")
+def read_item(slug: str):
+    return db.get(where("slug") == slugify(slug))
